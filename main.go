2b03a3ff100e2ea68ac59d99e9df26839faca5d8
Jon Staab <shtaab@gmail.com>
2024-02-01 11:57:20 -0800

    Get user data to load

--- main.go
+++ main.go
@@ -6,13 +6,46 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
+	"database/sql"
 	"github.com/fiatjaf/eventstore/sqlite3"
 	"github.com/fiatjaf/khatru"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/nbd-wtf/go-nostr"
 )
 
-func main() {
+func execSql(db *sql.DB, sql string) {
+	_, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getDb() *sql.DB {
+	db, err := sql.Open("sqlite3", "/tmp/smoxy-people.sqlite")
+	if err != nil {
+		panic(err)
+	}
+
+	execSql(db, `
+  	CREATE TABLE IF NOT EXISTS kind3 (
+    	pubkey TEXT PRIMARY KEY,
+      event TEXT
+  	);`,
+	)
+
+	execSql(db, `
+  	CREATE TABLE IF NOT EXISTS kind10003 (
+    	pubkey TEXT PRIMARY KEY,
+      event TEXT
+  	);`,
+	)
+
+	return db
+}
+
+func getEnv() func(k string, fallback ...string) (v string) {
 	var env = make(map[string]string)
 
 	for _, item := range os.Environ() {
@@ -20,7 +53,7 @@ func main() {
 		env[parts[0]] = parts[1]
 	}
 
-	getenv := func(k string, fallback ...string) (v string) {
+	return func(k string, fallback ...string) (v string) {
 		v = env[k]
 
 		if v == "" && len(fallback) > 0 {
@@ -29,17 +62,98 @@ func main() {
 
 		return v
 	}
+}
 
-	// create the relay instance
+type UserData struct {
+	Kind3     *nostr.Event
+	Kind10002 *nostr.Event
+}
+
+var users = make(map[string]UserData)
+
+func selectLatestEvent(a *nostr.Event, b *nostr.Event) *nostr.Event {
+	if a == nil {
+		return b
+	}
+
+	if b == nil {
+		return a
+	}
+
+	if a.CreatedAt > b.CreatedAt {
+		return a
+	} else {
+		return b
+	}
+}
+
+func loadUserData(pubkey string) {
+	// Set this so we don't try to fetch user data multiple times
+	users[pubkey] = UserData{}
+
+	ctx := context.Background()
+	relay, err := nostr.RelayConnect(ctx, "wss://purplepag.es")
+	if err != nil {
+		panic(err)
+	}
+
+	var filters = []nostr.Filter{{
+		Kinds:   []int{3, 10002},
+		Authors: []string{pubkey},
+		Limit:   10,
+	}}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	sub, err := relay.Subscribe(ctx, filters)
+	if err != nil {
+		panic(err)
+	}
+
+	var kind3 *nostr.Event
+	var kind10002 *nostr.Event
+
+	for ev := range sub.Events {
+		if ev.Kind == 3 {
+			kind3 = selectLatestEvent(kind3, ev)
+		}
+
+		if ev.Kind == 10002 {
+			kind10002 = selectLatestEvent(kind10002, ev)
+		}
+	}
+
+	users[pubkey] = UserData{kind3, kind10002}
+}
+
+func checkAuth(ctx context.Context) (reject bool, msg string) {
+	pubkey := khatru.GetAuthed(ctx)
+
+	if pubkey == "" {
+		return true, "auth-required: need to know who you are to proxy successfully"
+	}
+
+	if _, ok := users[pubkey]; !ok {
+		loadUserData(pubkey)
+	}
+
+	return false, ""
+}
+
+func main() {
+	db := getDb()
+	defer db.Close()
+
+	env := getEnv()
+
 	relay := khatru.NewRelay()
+	relay.Info.Name = env("RELAY_NAME")
+	relay.Info.Icon = env("RELAY_ICON")
+	relay.Info.PubKey = env("RELAY_PUBKEY")
+	relay.Info.Description = env("RELAY_DESCRIPTION")
 
-	// set up some basic properties (will be returned on the NIP-11 endpoint)
-	relay.Info.Name = getenv("RELAY_NAME")
-	relay.Info.PubKey = getenv("RELAY_PUBKEY")
-	relay.Info.Description = getenv("RELAY_DESCRIPTION")
-	relay.Info.Icon = getenv("RELAY_ICON")
-
-	backend := sqlite3.SQLite3Backend{DatabaseURL: "/tmp/smoxy.sqlite"}
+	backend := sqlite3.SQLite3Backend{DatabaseURL: "/tmp/smoxy-relay.sqlite"}
 	if err := backend.Init(); err != nil {
 		panic(err)
 	}
@@ -50,7 +164,7 @@ func main() {
 		},
 	)
 
-	relay.DeleteEvent = append(relay.StoreEvent,
+	relay.DeleteEvent = append(relay.DeleteEvent,
 		func(ctx context.Context, event *nostr.Event) error {
 			return backend.DeleteEvent(ctx, event)
 		},
@@ -58,19 +172,19 @@ func main() {
 
 	relay.QueryEvents = append(relay.QueryEvents,
 		func(ctx context.Context, filter nostr.Filter) (chan *nostr.Event, error) {
-			fmt.Println(khatru.GetAuthed(ctx))
 			return backend.QueryEvents(ctx, filter)
 		},
 	)
 
-	// you can request auth by rejecting an event or a request with the prefix "auth-required: "
+	relay.RejectEvent = append(relay.RejectEvent,
+		func(ctx context.Context, event *nostr.Event) (reject bool, msg string) {
+			return checkAuth(ctx)
+		},
+	)
+
 	relay.RejectFilter = append(relay.RejectFilter,
 		func(ctx context.Context, filter nostr.Filter) (reject bool, msg string) {
-			if khatru.GetAuthed(ctx) == "" {
-				return true, "auth-required: need to know who you are to proxy successfully"
-			}
-
-			return false, ""
+			return checkAuth(ctx)
 		},
 	)
 
@@ -82,7 +196,7 @@ func main() {
 		fmt.Fprintf(w, `This is a nostr relay, please connect using wss://`)
 	})
 
-	port := getenv("PORT", "3334")
+	port := env("PORT", "3334")
 
 	// start the server
 	fmt.Printf("running on :%s\n", port)
